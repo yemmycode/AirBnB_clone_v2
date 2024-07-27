3dae57f5ad4184619eebd5e86a1fa10bd8beb175
Yemmycode <yemiajibade17@gmail.com>
2024-07-27 11:53:18 -0700

    The AirBnB Webframe

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Launch a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """Render the main HBnB filters HTML page."""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """Close the current SQLAlchemy session."""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
